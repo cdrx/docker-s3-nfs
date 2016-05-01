800d7a682af95bb41ace27bdbcd3681b1361441d
Chris R <cdrx@users.noreply.github.com>
2016-05-01 13:54:15 +0100

    Update nfs-server.sh

--- nfs-server.sh
+++ nfs-server.sh
@@ -5,7 +5,7 @@ lsmod
 modprobe nfs
 modprobe nfsd
 
-echo "/storage   ${NFS_ACL-*}(fsid=0,rw,sync,insecure,no_subtree_check,anonuid=1000,anongid=1000,crossmnt)" > /etc/exports
+echo "/storage   ${NFS_ACL-*}(fsid=0,rw,no_root_squash,sync,insecure,no_subtree_check,anonuid=1000,anongid=1000,crossmnt)" > /etc/exports
 exportfs -rav
 service rpcbind restart
 service nfs-kernel-server restart
